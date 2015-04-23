e27cc7298c6773eeb0ca6cee5e9c6cb2eca89fc4
Щановский Максим Александрович <chshanovskiy.maxim@itmh.ru>
2015-04-23 17:10:17 +0500

    Добавлена новая проверка
    Теперь проверяется, чтобы содержимое тоже начиналось с большой буквы

--- src/check.php
+++ src/check.php
@@ -4,6 +4,7 @@ define('ERR_SUBJECT_REQUIRED', 'Сообщение должно содержат
 define('ERR_SUBJECT_TOO_LONG', 'Заголовок не должен превышать 50 символов');
 define('ERR_SUBJECT_REDUNDANT_DOT', 'Заголовок не должен заканчиваться точкой');
 define('ERR_SUBJECT_WRONG_CASE', 'Заголовок должен начинаться с большой буквы');
+define('ERR_MESSAGE_WRONG_CASE', 'Содержимое должно начинаться с большой буквы');
 
 /**
  * @param $text string Текст сообщения
@@ -23,19 +24,28 @@ function check($text)
         $errors[ERR_SUBJECT_REQUIRED] = 1;
     }
 
-    $subject = $data[0];
-    if (mb_strlen($subject) > 50) {
-        var_dump($subject, mb_strlen($subject));
-        $errors[ERR_SUBJECT_TOO_LONG] = 1;
+    $subject = @$data[0];
+    if ($subject !== null) {
+        if (mb_strlen($subject) > 50) {
+            $errors[ERR_SUBJECT_TOO_LONG] = 1;
+        }
+
+        if (mb_substr($subject, -1) === '.') {
+            $errors[ERR_SUBJECT_REDUNDANT_DOT] = 1;
+        }
+
+        $subject_first_letter = mb_substr($subject, 0, 1);
+        if ($subject_first_letter !== mb_convert_case($subject_first_letter, MB_CASE_UPPER)) {
+            $errors[ERR_SUBJECT_WRONG_CASE] = 1;
+        }
     }
 
-    if (mb_substr($subject, -1) === '.') {
-        $errors[ERR_SUBJECT_REDUNDANT_DOT] = 1;
-    }
-
-    $subject_first_letter = mb_substr($subject, 0, 1);
-    if ($subject_first_letter !== mb_convert_case($subject_first_letter, MB_CASE_UPPER)) {
-        $errors[ERR_SUBJECT_WRONG_CASE] = 1;
+    $message = @$data[1];
+    if ($message !== null) {
+        $message_first_letter = mb_substr($message, 0, 1);
+        if ($message_first_letter !== mb_convert_case($message_first_letter, MB_CASE_UPPER)) {
+            $errors[ERR_MESSAGE_WRONG_CASE] = 1;
+        }
     }
 
     mb_internal_encoding($internal_encoding);
